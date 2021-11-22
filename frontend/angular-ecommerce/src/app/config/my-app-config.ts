fd20137fdf1f45d017e0c9d6df177d8575b6c62e
Samay Deepak Naik <samsrdjnaik@gmail.com>
2021-11-22 23:40:22 +0530

    Working on Ecommerce Project based on Angular and SpringBoot

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc:{
+        clientId: '0oa2y85niaTnKiJm35d7',
+        issuer: 'https://dev-73087604.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile','email']
+    }
+}
